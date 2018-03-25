d0e281fcc9a21acb0dbf4da9254bd1ff709612d8
terminux <terminux@localhost.localdomain>
2018-03-25 19:45:27 +0300

    "another test"

--- readtemp.c
+++ readtemp.c
@@ -2,33 +2,36 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+int tempC;
+int readADC(char channel)
+{
+	ADMUX = (3 << REFS0) | (1 << ADLAR) | (channel << MUX0); // VREF=2.56V, 8-bit, channel #0 is on PA0
+	_delay_us(10);											 // allow multiplexer to settle
+	ADCSRA |= (1 << ADSC);									 // Start Conversion
+	while (ADCSRA & (1 << ADSC));		 // wait for completion
+	return ADCH; // 8-bit result because we use ADLAR
+}
 int main(void)
 {
+	DDRC = 0b11111111;
+	DDRD = 0b11111111;
 
-	//DDRB |= (1<<PINB0);
-	DDRD = 0xff;
-	ADCSRA = 0x8f;
-	ADMUX = 0X40;
-
+	ADCSRA = (1 << ADEN) | (1 << ADPS1) | (1 << ADPS2);
+	TIMSK = (1 << TOIE0); // enable timer overflow interrupt for Timer0
+	TCNT0 = 0x00;		  // set timer0 counter initial value to 0
+	TCCR0 = (1 << CS01);  // start timer0 with /8 prescaler
 	sei();
-
-	ADCSRA |= (1 << ADSC);
 
 	while (1)
 	{
-		//_delay_ms(1000);
-		//PORTB^=(1<<PINB0);
+		temp = readADC(0)
 	}
 
 	return 0;
 }
-
 ISR(ADC_vect)
 {
-	while ()
-	{
 
-		PORTD = ADCL;
-		ADCSRA |= (1 << ADSC);
-	}
+	PORTD = ADCL;
+	ADCSRA |= (1 << ADSC);
 }
