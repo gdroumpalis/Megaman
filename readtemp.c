f928058b541b00758983d58a93a31b07d0423328
gdroumpalis <georgiosdroubalis@hotmail.gr>
2018-03-25 20:18:03 +0300

    rollback

--- readtemp.c
+++ readtemp.c
@@ -2,36 +2,31 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-int temp;
-int readADC(char channel)
-{
-	ADMUX = (3 << REFS0) | (1 << ADLAR) | (channel << MUX0); // VREF=2.56V, 8-bit, channel #0 is on PA0
-	_delay_us(10);											 // allow multiplexer to settle
-	ADCSRA |= (1 << ADSC);									 // Start Conversion
-	while (ADCSRA & (1 << ADSC));		 // wait for completion
-	return ADCH; // 8-bit result because we use ADLAR
-}
 int main(void)
 {
-	DDRC = 0b11111111;
-	DDRD = 0b11111111;
 
-	ADCSRA = (1 << ADEN) | (1 << ADPS1) | (1 << ADPS2);
-	TIMSK = (1 << TOIE0); // enable timer overflow interrupt for Timer0
-	TCNT0 = 0x00;		  // set timer0 counter initial value to 0
-	TCCR0 = (1 << CS01);  // start timer0 with /8 prescaler
+	//DDRB |= (1<<PINB0);
+	DDRD = 0xff;
+	ADCSRA = 0x8f;
+	ADMUX = 0X40;
+
 	sei();
+
+	ADCSRA |= (1 << ADSC);
 
 	while (1)
 	{
-		temp = readADC(0);
+		//_delay_ms(1000);
+		//PORTB^=(1<<PINB0);
 	}
 
 	return 0;
 }
+
 ISR(ADC_vect)
 {
 
 	PORTD = ADCL;
 	ADCSRA |= (1 << ADSC);
+
 }
