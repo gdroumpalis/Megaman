6ea68353f1b4b0d2833020ddd29909dc2f4f69e8
terminux <georgiosdroumpalis@hotmail.gr>
2018-03-25 11:35:10 +0000

    Added basic files

---
+++ readtemp.c
@@ -0,0 +1,34 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+int main(void)
+{
+
+	//DDRB |= (1<<PINB0);
+	DDRD = 0xff;
+	ADCSRA = 0x8f;
+	ADMUX = 0X40;
+
+
+	sei();
+
+	ADCSRA|=(1 << ADSC);
+
+	while(1)
+	{
+		//_delay_ms(1000);
+		//PORTB^=(1<<PINB0);
+	}
+
+	return 0;
+
+
+
+}
+
+ISR(ADC_vect)
+{
+	PORTD=ADCL;
+	ADCSRA|=(1<<ADSC);
+}
