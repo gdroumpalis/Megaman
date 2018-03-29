05f44bcbbb9d4b5eec5ea597d28a2c55747e3545
terminux <georgiosdroumpalis@hotmail.gr>
2018-03-29 16:40:03 +0000

    Added FCPU

--- sendint.c
+++ sendint.c
@@ -1,7 +1,9 @@
 #include <avr/io.h>
 #include "util/delay.h"
 #include "avr/interrupt.h"
- 
+
+#define	F_CPU	1000000
+
 void usart_init()
 {
 	UBRRH = 0;
@@ -10,26 +12,35 @@ void usart_init()
 	UCSRC |= (1 << URSEL)|(3<<UCSZ0);
 }
 
-void usart_data_transmit(unsigned int data )
+void usart_data_transmit(unsigned char data )
 {
 	while ( !( UCSRA & (1<<UDRE)) );
 	UDR = data;
-	_delay_ms(1000);
+	_delay_ms(100);
 }
-unsigned int i = 0;
+
+void usart_string_transmit(char *string)
+{
+	while(*string)
+	{
+		usart_data_transmit(*string++);
+	}
+}
+
 int main(void)
 {
-    DDRB = 0XFF;
+    	//DDRD = 0XFF;
 	/* Replace with your application code */
 	usart_init();
 	//sei();
 	
 	while (1)
 	{
-		usart_data_transmit(i);
+
+		usart_string_transmit("Hello\n");
 		/*Transmits string to PC*/
 
 		usart_data_transmit(0x0d);
-		i++;
+		
 	}
 }
